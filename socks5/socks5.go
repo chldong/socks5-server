dcc515ce4decf4c42c2798c870a7dad104e7340d
netbyte <alex.tsai@protonmail.com>
2022-04-23 01:05:58 +0800

    code refactor

--- socks5/socks5.go
+++ socks5/socks5.go
@@ -1,95 +1,11 @@
 package socks5
 
-import (
-	"crypto/tls"
-	"io"
-	"log"
-	"net"
-)
-
-// Start socks5 server
+// Start server
 func StartServer(config Config) {
-	udpConn := handleUDP(config)
-	handleTCP(config, udpConn)
-}
-
-func handleTCP(config Config, udpConn *net.UDPConn) {
-	var l net.Listener
-	var err error
-	if config.TLS {
-		cer, err := tls.LoadX509KeyPair(config.ServerPem, config.ServerKey)
-		if err != nil {
-			log.Panic(err)
-		}
-		c := &tls.Config{Certificates: []tls.Certificate{cer}}
-		l, err = tls.Listen("tcp", config.LocalAddr, c)
-		if err != nil {
-			log.Panicf("[tls] failed to listen tcp %v", err)
-		}
-		log.Printf("socks5-server [tls] started on %s", config.LocalAddr)
-	} else {
-		l, err = net.Listen("tcp", config.LocalAddr)
-		if err != nil {
-			log.Panicf("[tcp] failed to listen tcp %v", err)
-		}
-		log.Printf("socks5-server [tcp] started on %s", config.LocalAddr)
-	}
-	for {
-		tcpConn, err := l.Accept()
-		if err != nil {
-			continue
-		}
-		go tcpHandler(tcpConn, udpConn, config)
-	}
-}
-
-func handleUDP(config Config) *net.UDPConn {
-	udpRelay := &UDPRelay{Config: config}
-	return udpRelay.Start()
-}
-
-func tcpHandler(tcpConn net.Conn, udpConn *net.UDPConn, config Config) {
-	buf := make([]byte, BufferSize)
-	// read version
-	n, err := tcpConn.Read(buf[0:])
-	if err != nil || err == io.EOF {
-		return
-	}
-	b := buf[0:n]
-	if b[0] != Socks5Version {
-		return
-	}
-	if config.Username == "" && config.Password == "" {
-		// no auth
-		responseAuthType(tcpConn, NoAuth)
-	} else {
-		// username and password auth
-		responseAuthType(tcpConn, UserPassAuth)
-		username, password := getUserPwd(tcpConn)
-		if username == config.Username && password == config.Password {
-			responseAuthStatus(tcpConn, AuthSuccess)
-		} else {
-			responseAuthStatus(tcpConn, AuthFailure)
-		}
-	}
-	// read cmd
-	n, err = tcpConn.Read(buf[0:])
-	if err != nil || err == io.EOF {
-		return
-	}
-	b = buf[0:n]
-	switch b[1] {
-	case ConnectCommand:
-		TCPProxy(tcpConn, b)
-		return
-	case AssociateCommand:
-		UDPProxy(tcpConn, udpConn, config)
-		return
-	case BindCommand:
-		responseClient(tcpConn, CommandNotSupported)
-		return
-	default:
-		responseClient(tcpConn, CommandNotSupported)
-		return
-	}
+	// start udp server
+	u := &UDPServer{config: config}
+	udpConn := u.Start()
+	// start tcp server
+	t := &TCPServer{config: config, udpConn: udpConn}
+	t.Start()
 }
