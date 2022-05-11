aad8f9e5b0d3ea5e3d22b0aa18f0399d4c4559be
kucai <kucai@myweimai.com>
2022-05-11 16:43:57 +0800

    add mac arm64

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,8 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./bin/socks5-server-linux-amd6
 CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o ./bin/socks5-server-linux-arm64 ./main.go
 #Mac amd64
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ./bin/socks5-server-darwin-amd64 ./main.go
+#Mac arm64
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o ./bin/socks5-server-darwin-arm64 ./main.go
 #Windows amd64
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ./bin/socks5-server-windows-amd64.exe ./main.go
 #Operwrt amd64
